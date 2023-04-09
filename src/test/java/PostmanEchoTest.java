f9e228e340061599a72e17fa9361803b1bcfc3c9
Julia Hlimonenko <707010@gmail.com>
2023-04-09 22:51:01 +0300

    add test fail

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -16,6 +16,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some another data"));
     }
 }
